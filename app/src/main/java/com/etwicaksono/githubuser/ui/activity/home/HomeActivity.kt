815e86ea6ec503922e3cbde4b64dfd9f14dc5791
Eko Teguh Wicaksono <echotechno007@gmail.com>
2022-08-27 11:28:10 +0700

    Change HomeActivity.kt initial code

--- app/src/main/java/com/etwicaksono/githubuser/ui/activity/home/HomeActivity.kt
+++ app/src/main/java/com/etwicaksono/githubuser/ui/activity/home/HomeActivity.kt
@@ -3,10 +3,15 @@ package com.etwicaksono.githubuser.ui.activity.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.etwicaksono.githubuser.R
+import com.etwicaksono.githubuser.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding= ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
