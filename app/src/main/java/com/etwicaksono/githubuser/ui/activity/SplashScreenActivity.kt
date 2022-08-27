b0ff9e7b1583442efe5970009b5d84cedd50fe07
Eko Teguh Wicaksono <echotechno007@gmail.com>
2022-08-27 11:25:38 +0700

    Setup SplashScreenActivity.kt

--- app/src/main/java/com/etwicaksono/githubuser/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/etwicaksono/githubuser/ui/activity/SplashScreenActivity.kt
@@ -1,14 +1,24 @@
 package com.etwicaksono.githubuser.ui.activity
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.etwicaksono.githubuser.R
+import com.etwicaksono.githubuser.ui.activity.home.HomeActivity
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity,HomeActivity::class.java))
+            finish()
+        },2000)
     }
 }
