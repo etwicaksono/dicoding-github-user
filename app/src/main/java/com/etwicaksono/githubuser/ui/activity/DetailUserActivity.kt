03ecd2eb4a757101965dd10745ba940cc70d4254
Eko Teguh Wicaksono <echotechno007@gmail.com>
2022-08-28 02:14:05 +0700

    Show image on detail page

--- app/src/main/java/com/etwicaksono/githubuser/ui/activity/DetailUserActivity.kt
+++ app/src/main/java/com/etwicaksono/githubuser/ui/activity/DetailUserActivity.kt
@@ -2,11 +2,26 @@ package com.etwicaksono.githubuser.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.etwicaksono.githubuser.R
+import com.etwicaksono.githubuser.databinding.ActivityDetailUserBinding
+import com.etwicaksono.githubuser.entity.UsersListItem
 
 class DetailUserActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_user)
+        binding = ActivityDetailUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val userIntent = intent.getParcelableExtra<UsersListItem>("user")
+
+        binding.apply {
+            Glide.with(this@DetailUserActivity)
+                .load(userIntent?.avatar)
+                .placeholder(R.drawable.default_image)
+                .into(ivUser)
+        }
     }
 }
