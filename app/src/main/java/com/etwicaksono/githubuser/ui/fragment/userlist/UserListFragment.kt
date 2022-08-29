4e0f37cff2d9aa978c2b67c7ba57af7b961be3ed
Eko Teguh Wicaksono <echotechno007@gmail.com>
2022-08-30 03:05:12 +0700

    Add UserListFragment.kt

---
+++ app/src/main/java/com/etwicaksono/githubuser/ui/fragment/userlist/UserListFragment.kt
@@ -0,0 +1,48 @@
+package com.etwicaksono.githubuser.ui.fragment.userlist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.etwicaksono.githubuser.R
+import com.etwicaksono.githubuser.databinding.FragmentUserListBinding
+
+private const val ARG_TYPE = "USER_LIST_TYPE"
+private const val ARG_USERNAME = "USER_LIST_USERNAME"
+
+class UserListFragment : Fragment() {
+
+    private var _binding: FragmentUserListBinding? = null
+    private val binding get() = _binding
+
+    private var type: String? = null
+    private var username: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            type = it.getString(ARG_TYPE)
+            username = it.getString(ARG_USERNAME)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       _binding=FragmentUserListBinding.inflate(inflater,container,false)
+        return binding?.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(type: String, username: String) =
+            UserListFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_TYPE, type)
+                    putString(ARG_USERNAME, username)
+                }
+            }
+    }
+}
